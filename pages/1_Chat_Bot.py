6a67e38ca3c712b613288f90c9f7d98ac6d8437f
Surya Prakash <suryaprakash.somasundaram@solitontech.local>
2025-03-03 01:22:57 +0530

    Adding chat bot
    Signed-off-by: Surya Prakash <suryaprakash.somasundaram@solitontech.local>

--- pages/1_Chat_Bot.py
+++ pages/1_Chat_Bot.py
@@ -41,9 +41,36 @@ s_engine = SubQuestionQueryEngine.from_defaults(query_engine_tools=query_engine_
 
 
 st.title("Product Specification Chatbot")
-query = st.text_input("Ask about PXIe-4139 or PXIe-4147:")
+# query = st.text_input("Ask about PXIe-4139 or PXIe-4147:")
 
-if query:
-    response = s_engine.query(query)  # Change based on product selection
-    st.write("**Answer:**", response.response)
-    st.write("**Source:**", response)
+# if query:
+#     response = s_engine.query(query)  # Change based on product selection
+#     st.write("**Answer:**", response.response)
+#     st.write("**Source:**", response)
+
+# Create a session state variable to store the chat messages. This ensures that the
+# messages persist across reruns.
+if "messages" not in st.session_state:
+    st.session_state.messages = []
+
+# Display the existing chat messages via `st.chat_message`.
+for message in st.session_state.messages:
+    with st.chat_message(message["role"]):
+        st.markdown(message["content"])
+
+# Create a chat input field to allow the user to enter a message. This will display
+# automatically at the bottom of the page.
+if prompt := st.chat_input("Ask about PXIe-4139 or PXIe-4147"):
+
+    # Store and display the current prompt.
+    st.session_state.messages.append({"role": "user", "content": prompt})
+    with st.chat_message("user"):
+        st.markdown(prompt)
+
+    response = s_engine.query(prompt)
+
+    # Stream the response to the chat using `st.write_stream`, then store it in 
+    # session state.
+    with st.chat_message("assistant"):
+        st.markdown(response.response)
+    st.session_state.messages.append({"role": "assistant", "content": response.response})
